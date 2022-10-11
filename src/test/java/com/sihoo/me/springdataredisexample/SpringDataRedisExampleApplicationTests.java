4473b35a135f47d8a364a8491f2fc3ead835197c
mikekang47 <gyeong0308@gmail.com>
2022-10-11 15:23:12 +0900

    Set environment

---
+++ src/test/java/com/sihoo/me/springdataredisexample/SpringDataRedisExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sihoo.me.springdataredisexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataRedisExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
